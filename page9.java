31dbf33581fcedce767e17c0f0b9dfad741e2ae1
izzyara <izzybzb@gmail.com>
2024-02-07 14:36:44 -0500

    page9

---
+++ page9.java
@@ -0,0 +1,16 @@
+
+/************************************
+*Project: Demo for Comments
+*Programmer: Izzy
+*Date: 2/7/2024
+*Program Name: page9.java
+*************************************/
+
+public class page9
+{
+        public static void main (String[] args)
+    {
+    System.out.println("hi");
+    System.out.println("LabOneA");
+    }
+}
